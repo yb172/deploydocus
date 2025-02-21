718578036827515994c4960d33b07c782c9add83
Yan Babitski <yan.babitski@gmail.com>
2025-02-21 01:02:31 -0800

    Update project name to "deploydocus"

--- main.go
+++ main.go
@@ -19,12 +19,12 @@ const port = 8081
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(fmt.Sprintf("Running server version: %s\n", getVersion())))
+		w.Write([]byte(fmt.Sprintf("Running 🦕 version: %s\n", getVersion())))
 	})
 
-	fmt.Printf("Starting server on :%d\n", port)
+	fmt.Printf("Starting 🦕 on :%d\n", port)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
-		log.Fatal("Unable to start server: ", err)
+		log.Fatal("Unable to start 🦕: ", err)
 	}
 }
