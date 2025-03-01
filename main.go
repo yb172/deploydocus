2e14f366357b1846261fc086abe668726506be6c
Yan Babitski <yan.babitski@gmail.com>
2025-02-28 23:35:44 -0800

    fix: fix bug on main page

--- main.go
+++ main.go
@@ -37,7 +37,6 @@ const pageTmpl = `
 	<body>
 		<pre style="word-wrap: break-word;">{{.Logo}}</pre>
 		<pre style="word-wrap: break-word; white-space: pre-wrap;">{{.Stats}}</pre>
-		Oh no, it's a bug!
 	</body>
 </html>`
 
